1f3a38b396616689c0a718f1e6cf0349a2ce3d64
anis-harmali <56651477+anis-harmali@users.noreply.github.com>
2019-10-21 15:20:10 +0200

    Update RelDef.java

--- Code/src/RelDef.java
+++ Code/src/RelDef.java
@@ -4,29 +4,46 @@ public class RelDef {
 	private String nom;
 	private int nbcol;
 	private List<String> type;
-	int fileIdx;
-	int recordSize;
-	int slotCount;
-	
-	
-	public RelDef(String nom, int nbcol, List<String> type,int fileIdx,int recordSize,int slotCount) {
-		this.nom=nom;
-		this.nbcol=nbcol;
-		this.type=type;
-		this.fileIdx=fileIdx;
-		this.recordSize=recordSize;
-		this.slotCount=slotCount;
-		
-	}
+	private int fileIdx;
+	private int recordSize;
+	private int slotCount;
 
+	public RelDef(String nom, int nbcol, List<String> type, int fileIdx, int recordSize, int slotCount) {
+		this.nom = nom;
+		this.nbcol = nbcol;
+		this.type = type;
+		this.fileIdx = fileIdx;
+		this.recordSize = recordSize;
+		this.slotCount = slotCount;
+
+	}
 
 	public List<String> getType() {
 		return type;
 	}
-	
-	
-	
-	
-	
-	
+
+	public void setFileIdx(int fileIdx) {
+		this.fileIdx = fileIdx;
+	}
+
+	public int getFileIdx() {
+		return fileIdx;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getNbcol() {
+		return nbcol;
+	}
+
+	public int getRecordSize() {
+		return recordSize;
+	}
+
+	public int getSlotCount() {
+		return slotCount;
+	}
+
 }
