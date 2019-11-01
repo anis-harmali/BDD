c5aef2f8c0b5e29cc327984732e2416cd25de200
admin pc <admin pc@DESKTOP-4L0D6JD>
2019-11-01 14:32:56 +0100

    Main

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -6,20 +6,28 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-			DBManager inst = DBManager.getInstance();
-			ByteBuffer buf = ByteBuffer.allocate(Constants.pageSize);
-			ByteBuffer buff = ByteBuffer.allocate(Constants.pageSize);
-			buff.putChar('d');
-			
-			inst.init();
-			DiskManager disk = DiskManager.getInstance();
+		Constants.chemin = args[0];
+		BufferManager b = BufferManager.getInstance();
+		DBManager inst = DBManager.getInstance();
+		ByteBuffer bu1 = null;
+		ByteBuffer bu2 = null;
+		ByteBuffer bu3 = null;
+					
+		inst.init();
+		DiskManager disk = DiskManager.getInstance();
 		
-			PageId i3= new PageId(3,2);
-			
-			BufferManager bm = BufferManager.getInstance();
-			System.out.println(bm.getPage(i3));
-			
-			
+		PageId pi1 = new PageId(1,3);
+		PageId pi2 = new PageId(0,15);
+		PageId pi3 = new PageId(1,4);
+		
+		
+		bu1 = b.getPage(pi1);
+		bu2 = b.getPage(pi2);
+		bu3 = b.getPage(pi3);
+		
+		System.out.print(bu1.getChar());
+		System.out.print(bu2.getChar());
+
 			
 			
 		}
