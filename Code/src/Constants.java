114174783a98c317f890cc8dcc8715670ccbf1b0
admin pc <admin pc@DESKTOP-4L0D6JD>
2019-11-01 16:36:00 +0100

    java

--- Code/src/Constants.java
+++ Code/src/Constants.java
@@ -2,6 +2,6 @@
 public class Constants {
 	public static final int pageSize = 4;
 	public static final int frameCount = 2;
-	public static String chemin;
+	public static final String chemin;
 			
 }
