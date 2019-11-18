cee09a7e0aae25e21724c7216bfbdcecfcf87bd3
anis-harmali <56651477+anis-harmali@users.noreply.github.com>
2019-11-18 16:02:28 +0100

    Update Main.java

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -7,29 +7,20 @@ import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		Constants.chemin=args[0];
+		Constants.chemin = args[0];
 
-        BufferManager b = BufferManager.getInstance();
-        DBManager dbmanager = DBManager.getInstance();
-        DiskManager disk = DiskManager.getInstance();
-        DBDef dbDef = DBDef.getInstance();
-        FileManager fileManager = FileManager.getInstance();
-        List<String> liste = new ArrayList<String>();
-        dbmanager.Clean();
-        liste.add("int");
-        liste.add("float");
-        liste.add("int");
-       
-        dbmanager.CreateRelation("R", 3, liste);
-       	RelDef relDef = new RelDef("R", 2, liste,1, 20, 5);
-        String[] commande = {"insert","R", "1","1.3","2"};
-        dbmanager.insert(commande);  
-        String[] commande2 = {"insert","R", "2","1.5","2"};
-        dbmanager.insert(commande2);
-        String[] commande3 = {"selectall","R"};
-        dbmanager.selectall(commande3);
-		}
-	
+		DBManager dbmanager = DBManager.getInstance();
+		dbmanager.Clean();
+
+		dbmanager.ProcessCommand("create R 3 int string3 int");
+		dbmanager.ProcessCommand("insert R 1 aab 2");
+		dbmanager.ProcessCommand("insert R 2 abc 2");
+		dbmanager.ProcessCommand("insert R 2 abc 2");
+		dbmanager.ProcessCommand("insert R 2 abc 2");
+		dbmanager.ProcessCommand("selectall R");
+
 	}
+
+}
