f1b6c2754d1320224a7c51b9e26a487614206868
lina-chabour <56792501+lina-chabour@users.noreply.github.com>
2019-12-13 13:31:54 +0100

    Update Main.java

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,27 +1,35 @@
 import java.io.IOException;
-import java.lang.ref.Cleaner.Cleanable;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		Constants.chemin = args[0];
-
+		ArrayList<Integer> list= new ArrayList<>();
 		DBManager dbmanager = DBManager.getInstance();
+		//dbmanager.Clean();
+		Scanner s = new Scanner(System.in);
+		String commande="";
+		boolean b=true;
+		do {
+			System.out.println("Entrez votre commande");
+			commande=s.nextLine();
+			String[] comm=commande.split(" ");
 		
-		dbmanager.Clean();
-		
-		dbmanager.ProcessCommand("create R 3 int string3 int");
-		dbmanager.ProcessCommand("insert R 1 aab 2");
-		dbmanager.ProcessCommand("insert R 2 abc 2");
-		dbmanager.ProcessCommand("create S 2 int int");
-		dbmanager.ProcessCommand("insert S 1 2");
-		dbmanager.ProcessCommand("join R S 1 1");
-		dbmanager.ProcessCommand("join R S 3 2");
-		
+		if(comm[0].equals("exit")) {
+			b=false;
+			dbmanager.finish();
+			
+		}
+		else {
+			dbmanager.ProcessCommand(commande);
+		}
+		}while(b==true);
+
 	}
 
 }
