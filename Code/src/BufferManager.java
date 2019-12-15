ac4adb9379c973dbb459928a602f9e151fc52216
ryan-ait <56827741+ryan-ait@users.noreply.github.com>
2019-12-15 17:36:04 +0000

    Update BufferManager.java

--- Code/src/BufferManager.java
+++ Code/src/BufferManager.java
@@ -2,78 +2,75 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
-
 public class BufferManager {
 	private static BufferManager INSTANCE;
 	private ArrayList<Frame> bufferpool;
+
 	private BufferManager() {
 		bufferpool = new ArrayList<Frame>();
-		for (int i=0; i<Constants.frameCount ; i++)	{
+		for (int i = 0; i < Constants.frameCount; i++) {
 			bufferpool.add(new Frame());
 		}
 	}
-	
+
 	public static synchronized BufferManager getInstance() {
-		
-		if(INSTANCE == null) {
-			INSTANCE =  new BufferManager();
+
+		if (INSTANCE == null) {
+			INSTANCE = new BufferManager();
 			return INSTANCE;
-		}
-		else 
+		} else
 			return INSTANCE;
-	}	
-	
-	public ByteBuffer getPage(PageId pageId) throws IOException 
-	{
-		
-		for(int i=0; i<bufferpool.size();i++) {
+	}
+
+	public ByteBuffer getPage(PageId pageId) throws IOException {
+
+		for (int i = 0; i < bufferpool.size(); i++) {
 			Frame fr = bufferpool.get(i);
-			if (fr.getPageId() == null) continue;
+			if (fr.getPageId() == null)
+				continue;
 			if (fr.getPageId().equals(pageId)) {
 				fr.incrementerPin();
 				return fr.getBuffer();
 			}
 		}
-		
-		for(int i=0; i<bufferpool.size();i++) { 
+
+		for (int i = 0; i < bufferpool.size(); i++) {
 			Frame fr = bufferpool.get(i);
-			if (fr.getPageId()==(null)) {
-			DiskManager.getInstance().ReadPage(pageId, fr.getBuffer());
-			fr.setPageId(pageId);
-			fr.incrementerPin();
-			return fr.getBuffer();
+			if (fr.getPageId() == (null)) {
+				DiskManager.getInstance().ReadPage(pageId, fr.getBuffer());
+				fr.setPageId(pageId);
+				fr.incrementerPin();
+				return fr.getBuffer();
 			}
 		}
-	
-		
-		for(int i=0; i<bufferpool.size();i++) { 
+
+		for (int i = 0; i < bufferpool.size(); i++) {
 			Frame fr = bufferpool.get(i);
-			//freePage(fr.getPageId(), 1);	
+			// freePage(fr.getPageId(), 1);
 			if (fr.getPinCount() == 0) {
-				if(fr.getDirty()==1) {
+				if (fr.getDirty() == 1) {
 					DiskManager.getInstance().Writepage(fr.getPageId(), fr.getBuffer());
 				}
-			
-				fr.setDirty(0) ;
-			    DiskManager.getInstance().ReadPage(pageId, fr.getBuffer());
-			    fr.setPageId(pageId);
-			    fr.incrementerPin();
-			    return fr.getBuffer();	
+
+				fr.setDirty(0);
+				DiskManager.getInstance().ReadPage(pageId, fr.getBuffer());
+				fr.setPageId(pageId);
+				fr.incrementerPin();
+				return fr.getBuffer();
 			}
-			
+
 		}
-		
+
 		System.out.println("bug BM");
 		return null;
-		
+
 	}
-			
-		
 
 	public void freePage(PageId pageId, int valdirty) {
-		for(int i=0; i<bufferpool.size();i++) {
+		for (int i = 0; i < bufferpool.size(); i++) {
 			Frame fr = bufferpool.get(i);
-			if (fr.getPageId() == null) continue;
+			if (fr.getPageId() == null)
+				continue;
 			if (fr.getPageId().equals(pageId)) {
 				fr.decrementerPin();
 				fr.setDirty(valdirty);
@@ -82,18 +79,18 @@ public class BufferManager {
 		}
 		System.out.println("pas trouve pour free");
 	}
-	
-	
+
 	public void flushAll() throws IOException {
-		for(int i=0; i<bufferpool.size();i++) {
+		for (int i = 0; i < bufferpool.size(); i++) {
 			Frame fr = bufferpool.get(i);
-			if(fr.getDirty()==1) {
+			if (fr.getDirty() == 1) {
 				DiskManager.getInstance().Writepage(fr.getPageId(), fr.getBuffer());
 			}
+			bufferpool.set(i, new Frame());
 			fr = new Frame();
 		}
 	}
-	
+
 	public void raz() throws IOException {
 		flushAll();
 	}
